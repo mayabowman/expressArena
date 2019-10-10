44406ee68947d7b620fdedc2eb0726cb38fae46a
Maya Bowman <mayashady1@gmail.com>
2019-10-10 11:25:29 -0400

    more exercises

--- app.js
+++ app.js
@@ -15,6 +15,68 @@ app.get('/pizza/pepperoni', (req, res) => {
   res.send('Your pizza is on the way!')
 })
 
+app.get('/hello', (req, res) => {
+  res.status(204).end()
+})
+
+app.get('/video', (req, res) => {
+  const video = {
+    title: 'Cats falling over',
+    description: '15 minutes of hilarious fun as cats fall over',
+    length: '15.40'
+  }
+  res.json(video)
+})
+
+app.get('/colors', (req, res) => {
+  const colors = [
+    {
+      name: "red",
+      rgb: "FF0000"
+    },
+    {
+      name: "green",
+      rgb: "00FF00"
+    },
+    {
+      name: "blue",
+      rgb: "0000FF"
+    }
+  ]
+  res.json(colors)
+})
+
+app.get('/grade', (req, res) => {
+  const { mark } = req.query
+
+  if(!mark) {
+    return res.status(400).send('Please provide a mark')
+  }
+
+  const numericMark = parseFloat(mark)
+  if(Number.isNaN(numericMark)) {
+    return res.status(400).send('Mark must be a numeric value')
+  }
+
+  if(numericMark < 0 || numericMark > 100) {
+    return res.status(400).send('Mark must be in range 0 to 100')
+  }
+
+  if (numericMark >= 90) {
+    return res.send('A')
+  }
+  
+  if(numericMark >= 80) {
+    return res.send('B')
+  }
+
+  if(numericMark >=70) {
+    return res.send('C')
+  }
+
+  res.send('F')
+})
+
 // // Drill 1
 
 // app.get('/sum', (req, res) => {
